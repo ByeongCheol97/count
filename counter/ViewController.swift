e839569e53492df88f8fc357ef08b24a7ad98e6b
D7703_13 <d7703_13@d7703-13ui-imac.local>
2019-03-21 12:14:37 +0900

    commit

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.text = String(count)
     }
-
-
+    @IBAction func but(_ sender: Any) {
+        count = count + 1
+        label.text = String(count)
+    }
 }
 
